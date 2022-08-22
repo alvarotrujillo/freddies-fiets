70fab1f71f93c4967091f5fb4bc696bc22861659
Alvaro Trujillo <alvaro.trujillo@me.com>
2022-08-22 13:45:21 +0200

    Added devise views

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
+# Devise Gem below
+gem "devise"
+
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
