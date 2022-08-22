5c6189690fa178db2930abc96ce8492c450509a2
Lucas Luz <lucas@calazansluz.com>
2022-08-22 17:27:01 +0200

    linking cloudinary to our project

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,12 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
+  # to be added to the form  <%= f.input :photo, as: :file %>
+    # def article_params
+      # params.require(:article).permit(:title, :body, :photo)
+    # end
+
 
   validates :name, presence: true
   validates :type, presence: true
